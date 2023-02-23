7f32566a5b7a2c9d043e326999e9e29bebc8e41f
Temirkhan-dev <konurbaevtb@gmail.com>
2023-02-24 00:35:13 +0600

    Added component Rating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag, P, Button } from '@/components';
+import { Htag, P, Button, Rating } from '@/components';
 import { Inter } from '@next/font/google';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
   return (
     <>
       <div>
@@ -17,6 +19,7 @@ export default function Home() {
         <P size="f16w200">Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
         <P size="f16w300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
         <P size="f16w700">Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
+        <Rating rating={rating} isEditable setRating={setRating} />
       </div>
     </>
   );
